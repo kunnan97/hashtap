56bdb7f061b269350f43fc16726f78ee0c47a5fb
pongzers <you@example.com>
2021-05-23 22:22:16 +0800

    Final full frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test10: Edit <code>src/App.js</code> and save to reload.
+          SUCESSFUL PIPELIEN YAY
         </p>
         <a
           className="App-link"
